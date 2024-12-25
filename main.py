83a1836eb865c105eac57efb131fff2f44170687
Daniel Shirazi <danielshirazi@mac.mynetworksettings.com>
2024-12-25 18:02:00 -0500

    created some shitty files that dont even work

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import flet as ft
 
+import firebase_admin
+from firebase_admin import credentials
+
+# Initialize Firebase Admin SDK with service account credentials
+cred = credentials.Certificate("/Users/danielshirazi/Downloads/bitebuddies-8d6f6-firebase-adminsdk-ald1c-229148601d.json")
+firebase_admin.initialize_app(cred)
+
+
+
+
 def main(page: ft.Page):
     page.title = "Lunch Matching App"
     page.add(ft.Text("Hello, world!"))
